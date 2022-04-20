fc631871f05f7f31b07432d2e05659f2f8d7f38d
Shoham Ben-Yosef <73764507+ShohamBenYosef@users.noreply.github.com>
2022-04-20 21:06:47 +0300

    Add files via upload

--- Maman12/lists_ad.c
+++ Maman12/lists_ad.c
@@ -2,24 +2,35 @@
 #include <stdlib.h>
 
 int main() {
-    char *str = NULL;
+    char *str = NULL, *tmp = NULL;
     int c;
     int size = 0, len = 0, sumOfalpanumeric = 0;
-
-    while ((c=getchar()) != EOF && c != '\n') {
-        if (len + 1 >= size)
-        {
-            size = size * 2 + 1;
-            str = realloc(str, sizeof(char)*size);
-            
+    
+    printf("This program counts alpha-numeric characters.\nplease type your string:\n");
+	/* Get characters from user */
+    while ((c=getchar()) != EOF && c != '\n') 
+    {
+        if (len + 1 >= size) {
+            size = size * 2 + 1;/**/
+            tmp = realloc(str, sizeof(char)*size);
+            if(tmp){
+            	str = tmp;
+            }
+            else{
+            	free(tmp);
+            	free(str);
+	    }
         }
+        /* Checking the input */
         if(isalnum(c))
         	sumOfalpanumeric++;
         str[len++] = c;
     }
+    
+    /* Set the string and return the values */
     if (str != NULL) {
         str[len] = '\0';
-        printf("%s\n", str);
+        printf("\nYour String is:\n%s\n", str);
         printf("sum of alpha = %d, total sum = %d\n", sumOfalpanumeric, len);
         free(str);
     }
