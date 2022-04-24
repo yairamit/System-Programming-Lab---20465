6876882d6a8fd619b8562dd524b156757990c3d3
Shoham Ben-Yosef <73764507+ShohamBenYosef@users.noreply.github.com>
2022-04-24 20:13:08 +0300

    Add files via upload

--- Maman12/lists_ad.c
+++ Maman12/lists_ad.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void lists_ad();
+
 int main() {
-    char *str = NULL, *tmp = NULL;
+    lists_ad(); 
+    return 0;
+}
+
+/*
+* lists_ad func - Get a string from the user,
+* calculates the amount of alphanumeric characters and prints it on the screen.
+* 
+*/
+void lists_ad(){
+	char *str = NULL, *tmp = NULL;
     int c;
     int size = 0, len = 0, sumOfalpanumeric = 0;
     
     printf("This program counts alpha-numeric characters.\nplease type your string:\n");
 	/* Get characters from user */
-    while ((c=getchar()) != EOF && c != '\n') 
+    while ((c=getchar()) != EOF) 
     {
         if (len + 1 >= size) {
-            size = size * 2 + 1;/**/
+            size = size * 2 + 1;
             tmp = realloc(str, sizeof(char)*size);
+            /* check the if realloc succeeded */
             if(tmp){
             	str = tmp;
             }
+            /* if realloc did not succeed we will free the memory we have already used and exit.*/
             else{
             	free(tmp);
             	free(str);
+            	exit(0);
 	    }
         }
         /* Checking the input */
@@ -30,10 +46,9 @@ int main() {
     /* Set the string and return the values */
     if (str != NULL) {
         str[len] = '\0';
-        printf("\nYour String is:\n%s\n", str);
-        printf("sum of alpha = %d, total sum = %d\n", sumOfalpanumeric, len);
+        printf("\nYour String is:\n\n%s\n\n", str);
+        printf("- - - - - - - - - - - - -\nsum of alphanumeric characters = %d, total sum of characters = %d\n", sumOfalpanumeric, len);
         free(str);
     }
-
-    return 0;
+	
 }
